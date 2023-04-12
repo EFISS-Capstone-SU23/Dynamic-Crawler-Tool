b99fbdde2ff4076415be5c40274eeae8909bf095
DAN3002 <dinhanh300229@gmail.com>
2023-04-12 13:35:58 +0700

    Add setMaxListeners

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import './config/mongoose.js';
 import extractAll from './app/extract-all-link/extractAll.js';
 
 const URL = 'https://boo.vn/';
-const MAX_INSTANCE = 10;
+const MAX_INSTANCE = 16;
+
+process.setMaxListeners(MAX_INSTANCE + 5);
 
 extractAll(URL, MAX_INSTANCE);
