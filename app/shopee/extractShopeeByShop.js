0351c3c108ec3746124e1c2b89380e0ec0f5928c
DAN3002 <dinhanh300229@gmail.com>
2023-05-10 16:27:50 +0700

    Init shopee ETL

---
+++ app/shopee/extractShopeeByShop.js
@@ -0,0 +1,3 @@
+export default async function extractShopeeByShop(shopId) {
+	console.log('extractShopeeByShop', shopId);
+}
---
+++ app/shopee/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-restricted-syntax */
+import getDriverArray from '../../utils/getDriverArray';
+import {
+	MAX_INSTANCE,
+} from '../../config/parram.js';
+import extractShopeeByShop from './extractShopeeByShop';
+
+const SHOP_IDS = [
+	'coolmate.vn',
+];
+
+const driverArr = getDriverArray(MAX_INSTANCE);
+try {
+	for (const shopId of SHOP_IDS) {
+		await extractShopeeByShop(shopId, driverArr);
+	}
+} finally {
+	driverArr.forEach((driver) => {
+		driver.quit();
+	});
+}
