d13c3482da2edf820fabee7be3a0a5c6de0ec693
DAN3002 <dinhanh300229@gmail.com>
2023-05-25 21:57:22 +0700

    Read all shopee user and process

--- app/shopee/migration/addCategoties.js
+++ app/shopee/migration/addCategoties.js
@@ -1,20 +1,17 @@
 /* eslint-disable no-restricted-syntax */
+import '../../../config/env.js';
 import '../../../config/mongoose.js';
 
 import axios from 'axios';
+import fs from 'fs';
 
 import Products from '../../../models/Products.js';
 import logger from '../../../config/log.js';
-import {
-	SHOPEE_HEADER,
-} from '../config/header.js';
-import {
-	delay,
-} from '../../../utils/delay.js';
 
-const MAX_CHUNK = 1;
+const USER_FOLDER = './app/shopee/config/shopeeUser/';
 
-const fetchProductData = async (id, url) => {
+// eslint-disable-next-line no-unused-vars
+const fetchProductData = async (id, url, header) => {
 	const [shopId, itemId] = url.split('-i.')[1].split('.');
 	try {
 		const URL_ENDPOINT = `https://shopee.vn/api/v4/item/get?itemid=${itemId}&shopid=${shopId}`;
@@ -22,7 +19,7 @@ const fetchProductData = async (id, url) => {
 		const {
 			data,
 		} = await axios.get(URL_ENDPOINT, {
-			headers: SHOPEE_HEADER,
+			headers: header,
 		});
 
 		if (!data.data) {
@@ -50,6 +47,17 @@ const fetchProductData = async (id, url) => {
 	}
 };
 
+const processForEachUser = (userHeader, products) => {
+	const { NAME } = userHeader;
+
+	while (true) {
+		if (!products) {
+			logger.info(`${NAME} has no product => Stop!`);
+			return null;
+		}
+	}
+};
+
 const mainMigration = async () => {
 	const allShopeeProducts = (await Products.getAllProductByDomain('shopee.vn'))
 		.filter((product) => !product.categories || !product.description)
@@ -59,18 +67,34 @@ const mainMigration = async () => {
 		}));
 
 	// split to sub array with MAX_CHUNK
-	const allShopeeProductsChunks = [];
-	for (let i = 0; i < allShopeeProducts.length; i += MAX_CHUNK) {
-		allShopeeProductsChunks.push(allShopeeProducts.slice(i, i + MAX_CHUNK));
-	}
+	// const allShopeeProductsChunks = [];
+	// for (let i = 0; i < allShopeeProducts.length; i += MAX_CHUNK) {
+	// 	allShopeeProductsChunks.push(allShopeeProducts.slice(i, i + MAX_CHUNK));
+	// }
 
-	for (const chunk of allShopeeProductsChunks) {
-		await Promise.all(chunk.map(({
-			_id,
-			url,
-		}) => fetchProductData(_id, url)));
-		await delay(3 * 1000);
-	}
+	// for (const chunk of allShopeeProductsChunks) {
+	// 	await Promise.all(chunk.map(({
+	// 		_id,
+	// 		url,
+	// 	}) => fetchProductData(_id, url)));
+	// 	await delay(3 * 1000);
+	// }
+
+	// Read all file in user folder
+	const users = [];
+	fs.readdirSync(USER_FOLDER).forEach((file) => {
+		const pathFile = `${USER_FOLDER}/${file}`;
+		const user = JSON.parse(fs.readFileSync(pathFile));
+		user.pathFile = pathFile;
+
+		users.push(user);
+	});
+
+	const userProcessPromises = users.map(async (user) => {
+		processForEachUser(user, allShopeeProducts);
+	});
+
+	await Promise.all(userProcessPromises);
 };
 
 mainMigration();
