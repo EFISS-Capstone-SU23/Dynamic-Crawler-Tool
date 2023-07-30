e6d0ee1367c4f9e6618e9a48cf7594b6d0387ddb
DAN3002 <dinhanh300229@gmail.com>
2023-07-31 01:40:44 +0700

    Fix log when stop crawl

--- app/extract-all-link/extractAll.js
+++ app/extract-all-link/extractAll.js
@@ -257,13 +257,14 @@ export default async function extractAll(params) {
 	params.logger = logger;
 	try {
 		await _extractAll(params, driverArray).then(() => {
+			logger.info('Finish extract all link.');
 			logger.info('Quit all driver');
 			quitAllDriver(driverArray);
 		});
 	} catch (error) {
+		logger.error('Error when extract all link');
+		logger.error(error);
 		logger.info('Quit all driver');
 		quitAllDriver(driverArray);
 	}
-
-	logger.info('Finish extract all link.');
 }
