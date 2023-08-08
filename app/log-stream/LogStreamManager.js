93937f29b0cffee84186b8b634c9d618115691d5
DAN3002 <dinhanh300229@gmail.com>
2023-08-08 15:16:30 +0700

    Handle null streamSocket

--- app/log-stream/LogStreamManager.js
+++ app/log-stream/LogStreamManager.js
@@ -3,26 +3,42 @@ import { streamSocket } from './setup.js';
 
 const LogStreamManager = {
 	emitVisitedURLs(visitedURLs, crawlId) {
+		if (!streamSocket) {
+			return;
+		}
+
 		streamSocket.emit(`visitedURLsData-${crawlId}`, {
 			visitedURLs,
 		});
 	},
 	emitQueue(queue, crawlId) {
+		if (!streamSocket) {
+			return;
+		}
 		streamSocket.emit(`queueData-${crawlId}`, {
 			queue,
 		});
 	},
 	emitLogData(logData, crawlId) {
+		if (!streamSocket) {
+			return;
+		}
 		streamSocket.emit(`logData-${crawlId}`, {
 			data: logData,
 		});
 	},
 	emitNumOfCrawledProduct(numOfCrawledProduct, crawlId) {
+		if (!streamSocket) {
+			return;
+		}
 		streamSocket.emit(`numOfCrawledProduct-${crawlId}`, {
 			numOfCrawledProduct,
 		});
 	},
 	emitNumOfCrawledImage(numOfCrawledImage, crawlId) {
+		if (!streamSocket) {
+			return;
+		}
 		streamSocket.emit(`numOfCrawledImage-${crawlId}`, {
 			numOfCrawledImage,
 		});
