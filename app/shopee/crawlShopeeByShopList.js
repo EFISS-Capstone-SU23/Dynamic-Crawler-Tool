a789aa8f8313985a63283c96362b1b8d2a2d6fd5
DAN3002 <dinhanh300229@gmail.com>
2023-08-10 18:57:48 +0700

    Add batch worker

--- app/shopee/crawlShopeeByShopList.js
+++ app/shopee/crawlShopeeByShopList.js
@@ -8,6 +8,8 @@ const DATA_PATH = './app/shopee/data/shopList_output.json';
 const CHECKED_URL_PATH = './cache/shopeeCheckedURL.json';
 const CHECKED_SHOP_ID_PATH = './cache/shopeeCheckedShopId.json';
 
+const WORKER_NUMBER = 2;
+
 const main = async () => {
 	const currentShopName = 'FACIOSHOP.COM';
 
@@ -31,10 +33,31 @@ const main = async () => {
 	}
 	console.log('shopInfo', shopInfo.length);
 
-	for (const shop of shopInfo) {
-		const shopName = `shopee-${shop.shopName}`;
-		await getShopData(parseInt(shop.shopId, 10), shopName, checkedURL, checkedShopId);
+	// suffle shopInfo
+	shopInfo.sort(() => Math.random() - 0.5);
+
+	// split shopInfo to WORKER_NUMBER parts
+	const shopInfoParts = [];
+	for (let i = 0; i < WORKER_NUMBER; i++) {
+		shopInfoParts.push([]);
 	}
+	for (let i = 0; i < shopInfo.length; i++) {
+		shopInfoParts[i % WORKER_NUMBER].push(shopInfo[i]);
+	}
+
+	const worker = async (shopInfoPart) => {
+		// random delay between 0 and 5s
+		const delayTime = Math.random() * 5000;
+		// eslint-disable-next-line no-promise-executor-return
+		await new Promise((resolve) => setTimeout(resolve, delayTime));
+
+		for (const shop of shopInfoPart) {
+			const shopName = `shopee-${shop.shopName}`;
+			await getShopData(parseInt(shop.shopId, 10), shopName, checkedURL, checkedShopId);
+		}
+	};
+	const workerPromise = shopInfoParts.map((shopInfoPart) => worker(shopInfoPart));
+	await Promise.all(workerPromise);
 };
 
 main();
